24bd74853d508fedb9df6b6118a4bb4109d7f037
Mostovik71 <mostovik71@gmail.com>
2022-03-05 14:51:13 +0300

    implemented exp(forward)

--- nn_lib/math_fns/exp.py
+++ nn_lib/math_fns/exp.py
@@ -1,6 +1,5 @@
 from typing import Tuple
 import numpy as np
-
 from nn_lib.math_fns.function import Function
 
 
@@ -9,13 +8,17 @@ class Exp(Function):
     Exponent function
     """
 
+    def __init__(self, arg):
+        self.arg = arg
+
     def forward(self) -> np.ndarray:
+
         """
         Compute exponent of the argument, i.e. e^(self.args[0].data)
 
         :return: exponent of the argument
         """
-        raise NotImplementedError   # TODO: implement me as an exercise
+        return np.exp(self.arg)
 
     def _backward(self, grad_output: np.ndarray) -> Tuple[np.ndarray]:
         """
@@ -25,3 +28,5 @@ class Exp(Function):
         :return: a tuple with a single value representing the gradient over the exponent argument
         """
         raise NotImplementedError   # TODO: implement me as an exercise
+if __name__ == '__main__':
+    print(Exp(2).forward())
